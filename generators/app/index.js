e6f6d8b5cb1e3225a0baa57cf32bd942b2c489ac
maurício sipmann <sipmann@gmail.com>
2018-09-23 17:42:20 -0400

    force build

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,7 +4,6 @@ module.exports = class extends Generator {
     constructor(args, opts) {
         super(args, opts);
 
-        
         this.argument('appname', { type: String, required: false });
         this.appname = this.options.appname;
     }
